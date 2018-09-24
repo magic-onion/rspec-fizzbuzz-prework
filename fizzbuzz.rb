71771ebddf411efb4f844f8987c9cffcd502114c
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-24 01:07:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(number)
     "Fizz"
   elsif (number % 5 == 0)
     "Buzz"
-  elsif ((number % 15) == 0) && (number % 5 == 0) && (number % 3 == 0)
+  elsif (number % 15 == 0) && (number % 5 == 0) && (number % 3 == 0)
     "FizzBuzz"
   else 
     nil
